b0a9e7bdf91793e6bafaaa497ca31ef2ad683138
Patricio Tellez <d.mad.duck@gmail.com>
2019-05-01 16:59:51 -0400

    Adds basic files for Mvc Configuration and security

---
+++ monolith-web-app/src/main/java/cl/aws/demo/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package cl.aws.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/bower_components/**").addResourceLocations("/bower_components/"); 
+        registry.addResourceHandler("/dist/**").addResourceLocations("/dist/");
+        registry.addResourceHandler("/plugins/**").addResourceLocations("/plugins/"); 
+    }
+}
