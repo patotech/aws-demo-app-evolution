86eb40dae796a9ac88be1588c7d5705c7c603b31
Patricio Tellez <d.mad.duck@gmail.com>
2019-05-02 01:55:15 -0400

    Adds script to pre-load a user to the database

--- monolith-web-app/src/main/resources/data.sql
+++ monolith-web-app/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 insert into ROLE (id, name) values (1, 'Administrator');
-insert into USER (id, password, username) values ( 1, '$2y$12$.RRzxHg7Ci4T/H87Ka52M.Eb/b/mMqkT9s8xlJUQQm1MBbMJDvslC', 'admin');
+insert into USER (id, password, username) values ( 1, '$2a$10$uQPwowHSr3M8jqzUO/uB/unrkYX6bCdz/4A7DRLZ9Q5hisqMmbJ.q', 'admin@demo.aws');
 insert into USER_ROLES (users_id, roles_id) values (1,1);
